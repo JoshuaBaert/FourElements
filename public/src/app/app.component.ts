acb953bc63730ed262dce08d25e08225d1014cea
Joshua Baert <jbghoring@gmail.com>
2017-07-31 17:38:03 -0500

    Init front end

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -2,8 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  template: `
+    <div>
+      
+    </div>
+  `
 })
 export class AppComponent {
   title = 'app works!';
---
+++ public/src/app/nav/nav.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  styleUrls: ['./nav.component.scss'],
+  template: `
+    
+  `
+})
+export class NavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

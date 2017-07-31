5945fa67cf07ab44b7a5889053468779c9ad57b0
Joshua Baert <jbghoring@gmail.com>
2017-07-31 17:05:28 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Joshua Baert on 07/31/2017.
+ */
+
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const express = require('express');
+
+
+const config = require('./config');
+
+
+const app = express();
+
+
+app.use(express.static('./public/dist'));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.listen(config.port,()=>{
+	console.log('listening on port ' + config.port )
+});

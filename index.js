a2b40a473a46fe7eaf9b2f9f55ac731262a83e3f
Joshua Baert <jbghoring@gmail.com>
2017-08-02 11:51:31 -0500

    Finishing touches with Brandon

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ app.use(express.static('./public/last-build'));
 app.use(bodyParser.json());
 app.use(cors());
 
+app.get('/api/test', (req, res, next) => {
+	res.json({live: true})
+})
+
 app.listen(config.port,()=>{
 	console.log('listening on port ' + config.port )
 });
